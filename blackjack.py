f1f01b524cd2f02236461d2b49644e02453aa964
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-04-06 11:07:07 -0400

    added function for taking bets

--- blackjack.py
+++ blackjack.py
@@ -92,3 +92,17 @@ class Chips:
 
     def lose_bet(self):
         self.total -= self.bet
+        
+# Function for taking bets
+def take_bet(chips):
+
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Please enter an integer')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
