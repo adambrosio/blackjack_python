a6a05093c20b49bd9171f014913224514e3ac8ae
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-04-06 10:33:20 -0400

    card class created

--- blackjack.py
+++ blackjack.py
@@ -5,3 +5,15 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
 playing = True
+# Card class
+class Card:
+
+    def __init__(self,suit,rank):
+        suit = self.suit
+        rank = self.rank
+
+    def __str__(self):
+        return self.rank + 'of' + self.suit
+
+
+
