eff2c3ca02f776e259fb74cfaba3744c6664618d
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-04-12 12:28:09 -0400

    was missing a continue and break in hit_or_stand()

--- blackjack.py
+++ blackjack.py
@@ -122,13 +122,13 @@ def hit_or_stand(deck,hand):
 
         if x[0].upper() == 'H':
             hit(deck,hand)
-
         elif x[0].upper() == 'S':
             print("Player stand. Dealer's turn.")
             playing = False
-
         else:
             print("Sorry invalid input. Please enter 'H' or 'S': ")
+            continue
+        break
 
 def show_some(player,dealer):
 
